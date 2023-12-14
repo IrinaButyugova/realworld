2c9557a516dba2a73cfca03f0ac78d94cc95a020
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-12-14 11:14:50 +0300

    Register action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+
+import { registerAction } from "../../store/actions/register.actions";
 
 @Component({
     selector: 'rw-register',
@@ -9,7 +12,7 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterComponent implements OnInit{
     form!: FormGroup;
 
-    constructor(private fb: FormBuilder){
+    constructor(private fb: FormBuilder, private store: Store){
 
     }
 
@@ -26,6 +29,6 @@ export class RegisterComponent implements OnInit{
     }
 
     onSubmit(): void{
-
+        this.store.dispatch(registerAction(this.form.value));
     }
 }
