046846123152968604d5950ed0016885ba8c2cb9
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-12-18 10:55:53 +0300

    Auth reducers update

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { catchError, map, of, switchMap } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
 
 import { AuthService } from "../services/auth.service";
 import { registerAction, registerFailureAction, registerSuccessAction } from "../actions/register.actions";
@@ -16,8 +17,8 @@ export class RegisterEffect {
                 map((currentUser: CurrentUserInterface) => {
                    return registerSuccessAction({currentUser})
                 }),
-                catchError(() => {
-                    return of(registerFailureAction())
+                catchError((errorResponse: HttpErrorResponse) => {
+                    return of(registerFailureAction({errors: errorResponse.error.errors}))
                 })
             )
         })
