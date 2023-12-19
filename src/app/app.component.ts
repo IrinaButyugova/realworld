05fe8fc5b861689cc29331189fbf3ab0f4b9d22e
IrinaButyugova <irina.butyugova@sharp-dev.net>
2023-12-19 13:21:31 +0300

    Topbar module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
+import { TopBarModule } from './shared/modules/topBar/topBar.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AuthModule],
+  imports: [CommonModule, RouterOutlet, AuthModule, TopBarModule],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
